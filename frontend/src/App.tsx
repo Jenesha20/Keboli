adaa31baa78a9dc5ffff995e95005839a65e328e
Jenesha20 <jeneshamalars@gmail.com>
2026-02-21 19:07:19 +0530

    Initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+<link href="/src/style.css" rel="stylesheet"></link>
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl bg-red-500 font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
